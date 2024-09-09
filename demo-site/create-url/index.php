2d47d315f4e5ed6174832ea0a339631f7a147cd9
Patryk Namyslak <patryk.namyslak23@gmail.com>
2024-09-09 18:44:29 +0100

    new commit

--- demo-site/create-url/index.php
+++ demo-site/create-url/index.php
@@ -2,10 +2,15 @@
 
 $url = $_POST['url'];
 if(filter_var($url, FILTER_VALIDATE_URL)){
+    $url = base64_encode($url);
+    // $url = base64_decode($url); Decode when fetching
+    $bytes = random_bytes(20);
+    // return the result to the user
     ?>
-    <input type="text" class="result" readonly value="[ADD THE RESULT HERE!]">
+    <input type="text" class="result" readonly value="<?php echo $url;?>">
     <?php
 }else{
+    // throw an error
     ?>
     <input type="text" class="result" readonly value="Invalid Link!">
     <?php
