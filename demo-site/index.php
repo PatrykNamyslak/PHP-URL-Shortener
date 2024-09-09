3f6dbebbd2b57261ac6e51f867f5833251b3f573
Patryk Namyslak <patryk.namyslak23@gmail.com>
2024-09-09 17:52:49 +0100

    An edit to the demo site

--- demo-site/index.php
+++ demo-site/index.php
@@ -1,4 +1,15 @@
-<?php
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script src="https://unpkg.com/htmx.org@2.0.2" integrity="sha384-Y7hw+L/jvKeWIRRkqWYfPcvVxHzVzn5REgzbawhxAuQGwX1XWe70vji+VSeHOThJ" crossorigin="anonymous" type="text/javascript"></script>
+<script src="https://unpkg.com/hyperscript.org@0.9.12" type="text/javascript"></script>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js" type="text/javascript"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP URL Shortener</title>
+</head>
+<body>
+    <h1>PHP URL Shortener</h1>
+    <form hx-post="./create-url/"></form>
+</body>
+</html>
