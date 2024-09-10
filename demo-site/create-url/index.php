c21a4495b041291aee8afa00ca44c8c4be8fe920
Patryk Namyslak <patryk@null.local>
2024-09-10 09:08:46 +0100

    A few changes

--- demo-site/create-url/index.php
+++ demo-site/create-url/index.php
@@ -2,8 +2,11 @@
 require_once '../assets/functions.php';
 $url = $_POST['url'];
 if(filter_var($url, FILTER_VALIDATE_URL)){
-    $url = base64_encode($url);
-    insertDataIntoDatabaseTable(connection:$database_connection, table:'links', data:[base64_decode($url), $url], query:'INSERT INTO `links` (Url,UrlHash) VALUES (?,?)');
+    $url = trim($url, 'https://');
+    $url = trim($url, '/');
+    echo $url;
+    // $url = base64_encode($url);
+    // insertDataIntoDatabaseTable(connection:$database_connection, table:'links', data:[base64_decode($url), $url], query:'INSERT INTO `links` (Url,UrlHash) VALUES (?,?)');
     // $url = base64_decode($url); Decode when fetching
     // return the result to the user
     ?>
